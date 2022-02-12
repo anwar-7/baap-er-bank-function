c78cf1e5d1e290ff2fc7186d0f273cb7d3b42322
anwarul1 <anwarjewel007@gmail.com>
2022-02-12 18:44:15 +0600

    2nd commit

--- js/banking.js
+++ js/banking.js
@@ -1,1 +1,10 @@
+document
+  .getElementById('deposit-button')
+  .addEventListener('click', function () {
+    // deposit input filed
+    const depositInput = document.getElementById('deposit-input');
+    const depositAmountText = depositInput.value;
+    const depositAmount = parseFloat(depositAmountText);
 
+    // update deposit total
+  });
