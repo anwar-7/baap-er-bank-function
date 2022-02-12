b865a609d6cab1eed8b31c8abb594c30665be446
anwarul1 <anwarjewel007@gmail.com>
2022-02-12 19:32:37 +0600

    withdraw balance complit

--- js/banking.js
+++ js/banking.js
@@ -31,4 +31,18 @@ document
     const withdrawInput = document.getElementById('withdraw-input');
     const withdrawAmountText = withdrawInput.value;
     const withdrawAmount = parseFloat(withdrawAmountText);
+
+    // update withdraw total
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const previousWithdrawTotalText = withdrawTotal.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawTotalText);
+
+    withdrawTotal.innerText = previousWithdrawTotal + withdrawAmount;
+
+    // update balance
+    const balanceTotal = document.getElementById('balance-total');
+    const previousBalanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalText);
+
+    balanceTotal.innerText = previousBalanceTotal - withdrawAmount;
   });
